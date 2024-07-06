41a055703352eb94cf640134fa9983ae04a4dca5
jeffli0720 <124337118+jeffli0720@users.noreply.github.com>
2024-07-06 07:57:28 -0400

    Updated landing page to show install button

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -3,13 +3,11 @@ import { useEffect, useState } from "react";
 
 import styles from "./LandingPage.module.css";
 
-function LandingPage() {
+function LandingPage({ isPWA }) {
 	const [isMobile, setIsMobile] = useState(isMobileDevice());
 
 	function isMobileDevice() {
-		return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-			navigator.userAgent
-		);
+		return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 	}
 
 	useEffect(() => {
@@ -28,14 +26,7 @@ function LandingPage() {
 		<>
 			<div className={styles.page}>
 				<div className={styles.logoWrapper}>
-					<img
-						className={styles.logo}
-						src={
-							process.env.PUBLIC_URL +
-							"/static/maskable_icon_x512.png"
-						}
-						alt="logo"
-					/>
+					<img className={styles.logo} src={process.env.PUBLIC_URL + "/static/maskable_icon_x512.png"} alt="logo" />
 					<h4>choobs</h4>
 				</div>
 				<div className={styles.schedulingSimplified}>
@@ -43,31 +34,26 @@ function LandingPage() {
 					<p>A better way to view your schedule</p>
 				</div>
 				<div className={styles.buttons}>
-					<Link to="/register" className={styles.getStarted}>
-						Get Started
-					</Link>
-					<Link to="/login" className={styles.login}>
-						Log In
-					</Link>
+					{!isMobile || isPWA ? (
+						<>
+							<Link to="/register" className={styles.getStarted}>
+								Get Started
+							</Link>
+							<Link to="/login" className={styles.login}>
+								Log In
+							</Link>
+						</>
+					) : (
+						<Link to="/download" className={styles.install}>
+							<span className={`${"material-symbols-rounded"}`}>&#xf090;</span>
+							<span>Install</span>
+						</Link>
+					)}
 				</div>
 				{isMobile ? (
-					<img
-						className={styles.screenshot}
-						src={
-							process.env.PUBLIC_URL +
-							"/static/screenshot_mobile.png"
-						}
-						alt="screenshot"
-					/>
+					<img className={styles.screenshot} src={process.env.PUBLIC_URL + "/static/screenshot_mobile.png"} alt="screenshot" />
 				) : (
-					<img
-						className={`${styles.screenshot} ${styles.desktop}`}
-						src={
-							process.env.PUBLIC_URL +
-							"/static/screenshot_desktop.png"
-						}
-						alt="screenshot"
-					/>
+					<img className={`${styles.screenshot} ${styles.desktop}`} src={process.env.PUBLIC_URL + "/static/screenshot_desktop.png"} alt="screenshot" />
 				)}
 			</div>
 		</>
